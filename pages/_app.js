864203dd1cf04e46d8df0889a50bd7f639020871
Kareem2002Shimes <kareem4shimas@gmail.com>
2022-12-31 20:31:07 +0200

    food-delivery-app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import store from "../redux/store";
+import { Provider } from "react-redux";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
+
+export default MyApp;
