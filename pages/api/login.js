6a97e53d8e1d24c674d4b7394d03ac481f354c8f
kareem <kareem4shimas@gmail.com>
2024-02-16 22:00:12 +0200

    update cors

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,23 +1,30 @@
-import cookie from "cookie";
+import cookie from 'cookie';
+import NextCors from 'nextjs-cors';
 
-const handler = (req, res) => {
-  if (req.method === "POST") {
+const handler = async (req, res) => {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  if (req.method === 'POST') {
     const { username, password } = req.body;
     if (
       username === process.env.ADMIN_USERNAME &&
       password === process.env.ADMIN_PASSWORD
     ) {
       res.setHeader(
-        "Set-Cookie",
-        cookie.serialize("token", process.env.TOKEN, {
+        'Set-Cookie',
+        cookie.serialize('token', process.env.TOKEN, {
           maxAge: 60 * 60,
-          sameSite: "strict",
-          path: "/",
+          sameSite: 'strict',
+          path: '/',
         })
       );
-      res.status(200).json("Succesfull");
+      res.status(200).json('Succesfull');
     } else {
-      res.status(400).json("Wrong Credentials!");
+      res.status(400).json('Wrong Credentials!');
     }
   }
 };
