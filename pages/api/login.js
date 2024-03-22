c9a22663ef6727f56dc9c3dc0765106975225326
kareem <kareem4shimas@gmail.com>
2024-03-23 01:24:57 +0200

    update

--- pages/api/login.js
+++ pages/api/login.js
@@ -14,18 +14,15 @@ const handler = async (req, res) => {
       username === process.env.ADMIN_USERNAME &&
       password === process.env.ADMIN_PASSWORD
     ) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       res.setHeader(
         'Set-Cookie',
         cookie.serialize('token', process.env.TOKEN, {
           maxAge: 60 * 60,
-          sameSite: 'strict',
+          sameSite: 'none',
           path: '/',
         })
       );
-      res.status(200).json({accessToken:process.env.TOKEN});
+      res.status(200).json({ token: process.env.TOKEN });
     } else {
       res.status(400).json('Wrong Credentials!');
     }
